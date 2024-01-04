2d470c93bd34c0e408984b1e153bd5cbf481d57f
debugtitan <alts.devs@gmail.com>
2024-01-04 08:28:19 +0000

    update to mail client

--- mailer/email.py
+++ mailer/email.py
@@ -1,37 +1,118 @@
 import smtplib
 import ssl
-from email.message import EmailMessage
+from email import charset as Charset
+from email import generator
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+from email.utils import formatdate
+from io import BytesIO, StringIO
 
 from mailer.config import AppConfig
+
+
+
+class MIMEMixin:
+    def as_string(self, unixfrom=False, linesep="\n"):
+        """Return the entire formatted message as a string.
+        Optional `unixfrom' when True, means include the Unix From_ envelope
+        header.
+
+        This overrides the default as_string() implementation to not mangle
+        lines that begin with 'From '. See bug #13433 for details.
+        """
+        fp = StringIO()
+        g = generator.Generator(fp, mangle_from_=False)
+        g.flatten(self, unixfrom=unixfrom, linesep=linesep)
+        return fp.getvalue()
+
+    def as_bytes(self, unixfrom=False, linesep="\n"):
+        """Return the entire formatted message as bytes.
+        Optional `unixfrom' when True, means include the Unix From_ envelope
+        header.
+
+        This overrides the default as_bytes() implementation to not mangle
+        lines that begin with 'From '. See bug #13433 for details.
+        """
+        fp = BytesIO()
+        g = generator.BytesGenerator(fp, mangle_from_=False)
+        g.flatten(self, unixfrom=unixfrom, linesep=linesep)
+        return fp.getvalue()
+
+
+
+class SafeMIMEText(MIMEMixin, MIMEText):
+    def __init__(self, _text, _subtype="plain", _charset=None):
+        self.encoding = _charset
+        MIMEText.__init__(self, _text, _subtype=_subtype, _charset=_charset)
+
+    def __setitem__(self, name, val):
+        MIMEText.__setitem__(self, name, val)
+
+    def set_payload(self, payload, charset=None):
+        if charset == "utf-8" and not isinstance(charset, Charset.Charset):
+            has_long_lines = any(
+                len(line.encode()) > 998
+                for line in payload.splitlines()
+            )
+            charset = Charset.Charset("utf-8")
+        MIMEText.set_payload(self, payload, charset=charset)
+
+class SafeMIMEMultipart(MIMEMixin, MIMEMultipart):
+    def __init__(
+        self, _subtype="mixed", boundary=None, _subparts=None, encoding=None, **_params
+    ):
+        self.encoding = encoding
+        MIMEMultipart.__init__(self, _subtype, boundary, _subparts, **_params)
+
+    def __setitem__(self, name, val):
+        MIMEMultipart.__setitem__(self, name, val)
 
 
 class EmailClient(AppConfig):
     """ Email Client"""
 
     __slots__ = ("subject","message","recipient_list")
-    def __init__(self, subject: str, message: str, recipient_list: list[str]):
-        """
-        Initialize the EmailClient with the provided subject, message, and recipient list.
-
-        :param subject: Subject of the email.
-        :param message: Body of the email.
-        :param recipient_list: List of email addresses to send the email to.
-        """
-
+    content_subtype = "plain"
+    mixed_subtype = "mixed"
+    encoding = Charset.Charset("utf-8")
+    def __init__(self, subject: str, message: str, to=None,bcc=None):
+        
         super().__init__()
         self.subject = subject
-        self.message = message
-        self.recipient_list = recipient_list
+        self.body = message
+        self.to = to
+        self.bcc = bcc
+        self.extra_headers =  {}
+
+
 
     def _set_message(self):
-        msg = EmailMessage()
+        msg = SafeMIMEText(self.body, self.content_subtype, self.encoding)
         msg["Subject"] = self.subject
-        msg["From"] = self.email
-        msg["To"] = ", ".join(self.recipient_list)
-        msg.set_content(self.message)
+        msg["From"] = self.extra_headers.get("From", self.email)
+        self._set_list_header_if_not_empty(msg, "To", self.to)
+        self._set_list_header_if_not_empty(msg, "Bcc", self.bcc)
+
+        header_names = [key.lower() for key in self.extra_headers]
+        if "date" not in header_names:
+            msg["Date"] = formatdate(localtime=True)
+        for name, value in self.extra_headers.items():
+            if name.lower() != "from":  # From is already handled
+                msg[name] = value
         return msg
+    
+    
+    def _set_list_header_if_not_empty(self, msg, header, values):
+        """
+        Set msg's header, either from self.extra_headers, if present, or from
+        the values argument.
+        """
+        if values:
+            try:
+                value = self.extra_headers[header]
+            except KeyError:
+                value = ", ".join(str(v) for v in values)
+            msg[header] = value
 
     def _send_mail(self):
         """ send email """
