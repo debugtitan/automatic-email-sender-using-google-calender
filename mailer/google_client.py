f2a8fccbfbc4d7339a22d188a259103e5d644a7c
debugtitan <alts.devs@gmail.com>
2024-01-05 19:18:10 +0000

    adding  calender events

--- mailer/google_client.py
+++ mailer/google_client.py
@@ -3,8 +3,11 @@ from mailer.config import AppConfig
 
 
 service = build("calendar","v3",developerKey=AppConfig.google_api_key)
+
 class GoogleCalenderClient:
     """ Google Calender Client """
 
-    def __init__(self):
+    @staticmethod
+    def get_calender_events(self):
+        
         
