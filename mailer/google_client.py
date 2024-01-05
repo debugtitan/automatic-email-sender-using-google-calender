7437e0d4f4b9faea67be51e091ae399dcd21eac4
debugtitan <alts.devs@gmail.com>
2024-01-05 19:15:32 +0000

    setting up google client service

--- mailer/google_client.py
+++ mailer/google_client.py
@@ -1,8 +1,10 @@
 from googleapiclient.discovery import build
 from mailer.config import AppConfig
 
+
+service = build("calendar","v3",developerKey=A)
 class GoogleCalenderClient:
     """ Google Calender Client """
 
     def __init__(self):
-        pass
+        
