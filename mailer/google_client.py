bf1562975e234ec30851f266469f788689ea68e9
debugtitan <alts.devs@gmail.com>
2024-01-06 05:58:30 +0000

    changed google api client cls

--- mailer/google_client.py
+++ mailer/google_client.py
@@ -1,14 +1,19 @@
+import os
 from googleapiclient.discovery import build
-from mailer.config import AppConfig
+from google_auth_oauthlib.flow import InstalledAppFlow
+from mailer.config import  BASE_DIR
 
 
-service = build("calendar","v3",developerKey=AppConfig.google_api_key)
+SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+CLIENT_SECRETS_FILE = os.path.join(BASE_DIR, "secrets.json")
 
-class GoogleCalenderClient:
-    """ Google Calender Client """
+flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRETS_FILE, SCOPES)
+credentials = flow.run_local_server(port=0)
+service = build('calendar', 'v3', credentials=credentials)
+
+class GoogleCalendarClient:
+    """ Google Calendar Client """
 
     @staticmethod
-    def get_calender_list():
-        return service.calendarList().list(pageToken=None).execute()
-
-        
+    def get_calendar_list():
+        return service.calendarList().list().execute()
